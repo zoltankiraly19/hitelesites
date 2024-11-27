9b45a63e266e0eb925f5ec1b0b57db4b0a54f288
zoltankiraly19 <zoltan.kiraly@dunaelektronika.com>
2024-11-27 09:40:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ CORS(app)
 # IBM Cloud Object Storage konfigurálása
 cos = ibm_boto3.client(
     's3',
-    ibm_api_key_id='i4LdD__uysZhfV-WPzWvzH8kKr_gq7K7KX55DPVkZHes',
-    ibm_service_instance_id='4dd2087c-b236-4cb9-9089-faa38562f64b',
+    ibm_api_key_id='5o6X835azJMALPLiebgIUUqRQ8e-NEM_PkQJ4thH9aI7',
+    ibm_service_instance_id='f39973c6-786a-459c-9564-40f6d8e6a6b7',
     config=Config(signature_version='oauth'),
-    endpoint_url='https://s3.direct.us-south.cloud-object-storage.appdomain.cloud'
+    endpoint_url='https://s3.us-south.cloud-object-storage.appdomain.cloud'
 )
 
-bucket_name = 'servicenow2'  # COS bucket neve
+bucket_name = 'servicenow3'  # COS bucket neve
 
 # Segédfüggvény assignment groupok és prioritások lekérésére és tárolására
 def load_and_store_options(headers):
